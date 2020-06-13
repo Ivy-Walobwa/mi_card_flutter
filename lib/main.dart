5bf98f9487e37d541379b07e13d44f3a23acb569
Ivy Walobwa <ivywalobwa@gmail.com>
2020-06-13 09:43:30 +0300

    adding material icons

--- lib/main.dart
+++ lib/main.dart
@@ -32,11 +32,56 @@ class MyApp extends StatelessWidget {
               Text(
                 'FLUTTER DEVELOPER',
                 style: TextStyle(
-                  fontFamily: 'Source Sans Pro',
-                  color: Colors.brown[100],
-                  fontSize: 20.0,
-                  fontWeight: FontWeight.bold,
-                  letterSpacing: 2.5
+                    fontFamily: 'Source Sans Pro',
+                    color: Colors.brown[100],
+                    fontSize: 20.0,
+                    fontWeight: FontWeight.bold,
+                    letterSpacing: 2.5),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.phone,
+                      color: Colors.brown,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      '+254788888888',
+                      style: TextStyle(
+                          color: Colors.brown[900],
+                          fontSize: 20.0,
+                          fontFamily: 'Source Sans Pro'),
+                    )
+                  ],
+                ),
+              ),
+              Container(
+                color: Colors.white,
+                padding: EdgeInsets.all(10.0),
+                margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 25.0),
+                child: Row(
+                  children: <Widget>[
+                    Icon(
+                      Icons.email,
+                      color: Colors.brown,
+                    ),
+                    SizedBox(
+                      width: 10.0,
+                    ),
+                    Text(
+                      'ivywalobwa@gmail.com',
+                      style: TextStyle(
+                          color: Colors.brown[900],
+                          fontFamily: 'Sans Source Pro',
+                          fontSize: 20.0),
+                    ),
+                  ],
                 ),
               ),
             ],
